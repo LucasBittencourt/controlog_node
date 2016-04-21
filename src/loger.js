76a35204939d647df7edf45b79586637a95904be
unknown <ruanrafael.oliveira@gmail.com>
2016-04-20 22:36:29 -0300

    autenticacao no node para receber login e senha

--- src/loger.js
+++ src/loger.js
@@ -1,5 +1,20 @@
+var auth = require('basic-auth')
+var sha1 = require('node-sha1');
+
 var loger = function(req,res,next){
-	next();
+	var module = require('./controlog/models/client-schema');
+	var user = auth(req);
+
+	if(user.pass){
+		user.pass = sha1(user.pass);
+	}
+	module.Client.find({ email: user.name, password: user.pass }, function(error, data){
+		if(error || data.length ==0){
+			res.status(500).send('Autenticação Inválida!');
+		} else {
+			next();
+		}
+	});
 }
 
 module.exports = loger;
