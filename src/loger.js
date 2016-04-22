f84b386c0e51bd7a0f9a83dfbe31e41958843f35
Roberto Rabello <robertomdk@gmail.com>
2016-04-22 13:34:53 -0300

    permitindo o acesso ao dados sem passar o base64

--- src/loger.js
+++ src/loger.js
@@ -4,17 +4,20 @@ var sha1 = require('node-sha1');
 var loger = function(req,res,next){
 	var module = require('./controlog/models/client-schema');
 	var user = auth(req);
-
-	if(user.pass){
+ //TODO hack para passar sem autenticar. Remover isso
+	if(user){
 		user.pass = sha1(user.pass);
-	}
-	module.Client.find({ email: user.name, password: user.pass }, function(error, data){
-		if(error || data.length ==0){
-			res.status(500).send('Autenticação Inválida!');
-		} else {
-			next();
-		}
-	});
+
+		module.Client.find({ email: user.name, password: user.pass }, function(error, data){
+			if(error || data.length ==0){
+				res.status(500).send('Autenticação Inválida!');
+			} else {
+				next();
+			}
+		});
+	}else{
+		next();
+	} 
 }
 
 module.exports = loger;
